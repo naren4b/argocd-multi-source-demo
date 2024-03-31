6fdd31e9c1f1d919159536a00853d98fbbaca153
Narendranath Panda (Nokia) <narendranath.panda@nokia.com>
2024-03-31 09:24:34 +0530

    aded argocd app decoder

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Multiple Sources for an ArgoCD Application
 
-ref: Argo CD has the ability to specify multiple sources for a single Application. Argo CD compiles all the sources and reconciles the combined resources. 
+ref: Argo CD has the ability to specify multiple sources for a single Application. Argo CD compiles all the sources and reconciles the combined resources.
 (argocd-multi-sources)[https://argo-cd.readthedocs.io/en/stable/user-guide/multiple_sources/]
 
 # Add Git repo
@@ -10,14 +10,14 @@ ref: Argo CD has the ability to specify multiple sources for a single Applicatio
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # edit and add the argocd-server args - --insecure
-kubectl get secrets -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo 
+kubectl get secrets -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
 # Install argocd CLI
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
-# ArgoCD CLI login 
+# ArgoCD CLI login
 argocd login localhost:8080  --insecure
 
 USERNAME=npanda
@@ -32,16 +32,47 @@ argocd repo add ${REPO_URL} --username ${USERNAME} --password ${TOKEN}
 REPO_URL=https://github.com/naren4b/argocd-multi-source-demo.git
 argocd repo add ${REPO_URL} --username ${USERNAME} --password ${TOKEN}
 ```
-![Repos-added](image.png)
 
-# Deploy the App 
+![Repos-added](res/image.png)
+
+# Deploy the App
+
 ```bash
 git clone https://github.com/naren4b/argocd-multi-source-demo.git
-kubectl  apply -f dev-demo-argo-application.yaml -f staging-demo-argo-application.yaml
+kubectl  apply -f app/dev-demo-argo-application.yaml -f app/staging-demo-argo-application.yaml
 ```
 
-# Check the manifest file & match with the values given 
-kubectl get cm -n demo-dev demo -o jsonpath="{.data.env}" && echo 
-kubectl get cm -n demo-staging demo -o jsonpath="{.data.env}" && echo 
+# Check the manifest file & match with the values given
 
-![argo-apps](image-1.png)
+kubectl get cm -n demo-dev demo -o jsonpath="{.data.env}" && echo
+kubectl get cm -n demo-staging demo -o jsonpath="{.data.env}" && echo
+
+![argo-apps](res/image-1.png)
+
+# To check the templating is OK before applying
+
+```bash
+cat <<EOF >my-app.yaml
+project: default
+destination:
+  server: 'https://kubernetes.default.svc'
+  namespace: demo-dev
+sources:
+  - repoURL: 'https://github.com/naren4b/demo-app.git'
+    path: helm-chart
+    targetRevision: main
+    helm:
+      valueFiles:
+        - $values/env/dev-values.yaml
+  - repoURL: 'https://github.com/naren4b/argocd-multi-source-demo.git'
+    targetRevision: main
+    ref: values
+
+EOF
+```
+
+# Test the ArgoCD Application
+
+```bash
+python decodeArgoApp.py --app my-app --dry-run=true
+```
---
+++ decodeArgoApp.py
@@ -0,0 +1,112 @@
+# alias helmtester="python $PWD/decodeArgoApp.py  --dry-run=true "
+# helmtester --app=main-app-dc-deployments-active
+# python decodeArgoApp.py --app main-app-dc-deployments-active --dry-run=true > out.yaml
+
+
+import yaml
+import os
+import argparse
+
+isDryRun = "true"
+current_directory = os.getcwd()
+APPLICATION_NAME = "demo"
+
+
+def executeMyCommand(command):
+    # print("isDryRun :" + isDryRun)
+    print("# " + command)
+    if "false" == isDryRun:
+        os.system(command)
+
+
+def load_yaml_file(file_path):
+    with open(file_path, "r") as file:
+        try:
+            yaml_data = yaml.safe_load(file)
+            return yaml_data
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
+
+
+def replaceRefValue(sources, filepath):
+    ref = filepath.split("/")[0]
+    ref_dir = ""
+    for repo in sources:
+        if ref in repo.keys():
+            if ref[1:] in repo.get("ref"):
+                ref_dir = repo.get("repoURL").split("/")[4].split(".")[0]
+    return filepath.replace(ref, ref_dir)
+
+
+def getHelmCommand(sources):
+    HELM_TEMPLATE_BASE_COMMAND = "helm template"
+    FILE_PATH = ""
+    for repo in sources:
+        if "path" in repo:
+            TEMPLATE_PATH = (
+                repo.get("repoURL").split("/")[4].split(".")[0] + "/" + repo.get("path")
+            )
+
+            for key, values in repo.items():
+                if "helm" == key:
+                    for key, files in values.items():
+                        for filepath in files:
+                            # print(filepath)
+                            FILE_PATH = (
+                                FILE_PATH + " -f " + replaceRefValue(sources, filepath)
+                            )
+    executeMyCommand(
+        HELM_TEMPLATE_BASE_COMMAND
+        + " "
+        + APPLICATION_NAME
+        + " "
+        + TEMPLATE_PATH
+        + " "
+        + FILE_PATH
+    )
+
+
+def getGitCommand(sources):
+    for repo in sources:
+        GIT_CLONE_BASE_COMMAND = "git clone --depth 1 -b "
+        ref_dir = repo.get("repoURL").split("/")[4].split(".")[0]
+        repoURL = repo.get("repoURL")
+        targetRevision = repo.get("targetRevision")
+        if os.path.exists(format(os.path.join(current_directory, ref_dir))):
+            executeMyCommand("cd  {}".format(os.path.join(current_directory, ref_dir)))
+            executeMyCommand("git pull origin " + targetRevision)
+        else:
+            executeMyCommand(
+                GIT_CLONE_BASE_COMMAND + " " + targetRevision + " " + repoURL
+            )
+
+
+def main():
+    data = load_yaml_file(APPLICATION_NAME + ".yaml")
+    sources = data.get("sources")
+    if data is None:
+        print("Failed to load YAML file.")
+        return
+    getGitCommand(sources)
+    getHelmCommand(sources)
+
+
+if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Process command-line arguments")
+    # Add arguments
+    parser.add_argument("--app", required=True, help="Name of the application")
+    parser.add_argument(
+        "--dry-run",
+        choices=["true", "false"],
+        required=True,
+        help="Dry run mode (true/false)",
+    )
+
+    # Parse arguments
+    args = parser.parse_args()
+    APPLICATION_NAME = args.app
+    isDryRun = args.dry_run
+    # Call main function with parsed arguments
+    main()

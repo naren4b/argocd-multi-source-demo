d514af0d7e937a2bc8932eb72e1f97ac207870ac
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2024-09-16 13:55:08 +0530

    Create demo-app.sh

---
+++ demo-app.sh
@@ -0,0 +1,18 @@
+SERVER="https://edge-1-control-plane:6443"
+
+cat<<EOF | kubectl apply  -n argocd -f -
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: 'demo-app'
+spec:
+  destination:
+    server: $SERVER
+    namespace: default
+  source:
+    path: sample-app
+    repoURL: https://github.com/naren4b/demo-app.git
+    targetRevision: HEAD  
+  project: default
+EOF
+
